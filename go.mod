9823748f1ad6386debe68b2a6b072c8431ad20b4
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-28 21:15:39 +0900

    feat: add fyne project

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kaleidot725/srccpy-hub
+
+go 1.12
+
+require fyne.io/fyne v1.4.2
---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fyne.io/fyne/app"
+	"fyne.io/fyne/widget"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Hello")
+
+	hello := widget.NewLabel("Hello Fyne!")
+	w.SetContent(widget.NewVBox(
+		hello,
+		widget.NewButton("Hi!", func() {
+			hello.SetText("Welcome :)")
+		}),
+	))
+
+	w.ShowAndRun()
+}
