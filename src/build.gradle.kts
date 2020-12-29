83aa24e4941d6921ea1fa7a5a1f7f6901a9e35f3
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-29 12:54:03 +0900

    feat: add compose for desktop project

---
+++ src/build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.compose.compose
+import org.jetbrains.compose.desktop.application.dsl.TargetFormat
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.4.20"
+    id("org.jetbrains.compose") version "0.2.0-build132"
+}
+
+group = "me.kaleidot725"
+version = "1.0"
+
+repositories {
+    jcenter()
+    mavenCentral()
+    maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+}
+
+dependencies {
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
+    implementation(compose.desktop.currentOs)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.desktop {
+    application {
+        mainClass = "MainKt"
+        nativeDistributions {
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb)
+            packageName = "ScrcpyHub"
+        }
+    }
+}
