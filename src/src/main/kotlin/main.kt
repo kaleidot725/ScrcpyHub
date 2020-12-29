8880296cc9dc9da43e501e5fa575ad5250727af9
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-29 22:28:57 +0900

    feat: modify main window visibility

--- src/src/main/kotlin/main.kt
+++ src/src/main/kotlin/main.kt
@@ -1,17 +1,23 @@
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
 import model.repository.DeviceRepository
 import model.repository.ResolutionRepository
 import view.DeviceListView
 import view.ResolutionListView
 
-fun main() = Window {
+fun main() = Window(
+    size = IntSize(250, 600)
+) {
     val resolutionRepository = ResolutionRepository()
     val deviceRepository = DeviceRepository()
 
     MaterialTheme {
-        Column {
+        Column(modifier = Modifier.padding(vertical = 8.dp, horizontal = 8.dp)) {
             ResolutionListView(resolutionRepository)
             DeviceListView(deviceRepository)
         }
