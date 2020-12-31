8fa31d52eff2f5d2a2068b5245efa61f990f9767
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-12-31 23:32:01 +0900

    feat: run and stop scrcpy

--- src/src/main/kotlin/main.kt
+++ src/src/main/kotlin/main.kt
@@ -6,6 +6,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import command.AdbCommand
+import command.ScrcpyCommand
 import model.repository.DeviceRepository
 import model.repository.ResolutionRepository
 import resource.Strings
@@ -18,14 +19,15 @@ fun main() = Window(
     title = Strings.APP_NAME
 ) {
     val adbCommand = AdbCommand()
+    val scrcpyCommand = ScrcpyCommand()
     val resolutionRepository = ResolutionRepository()
-    val deviceRepository = DeviceRepository(adbCommand)
+    val deviceRepository = DeviceRepository(adbCommand, scrcpyCommand)
 
     MaterialTheme {
         Column(modifier = Modifier.padding(8.dp)) {
             DeviceListView(deviceRepository)
             ResolutionListView(resolutionRepository)
-            RunAndStopButton()
+            RunAndStopButton(deviceRepository)
         }
     }
 }
