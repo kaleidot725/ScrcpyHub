43b09a0a1113b78ea5e538c00c34c3624ce171d1
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-03 22:21:58 +0900

    refactor: monitor process

---
+++ src/src/main/kotlin/extension/ProcessExtension.kt
@@ -0,0 +1,17 @@
+package extension
+
+import kotlinx.coroutines.delay
+
+suspend fun Process.waitForRunning(interval: Long) {
+    while (!this.isAlive) {
+        delay(interval)
+    }
+}
+
+suspend fun Process.monitor(interval: Long, onDestroy: suspend () -> Unit) {
+    while (this.isAlive) {
+        delay(interval)
+    }
+
+    onDestroy.invoke()
+}
