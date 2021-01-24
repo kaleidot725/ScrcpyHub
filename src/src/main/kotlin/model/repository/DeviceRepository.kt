d618192991d47703b3712a851d656b43d6570089
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-24 16:35:33 +0900

    feat: update device repository interface

--- src/src/main/kotlin/model/repository/DeviceRepository.kt
+++ src/src/main/kotlin/model/repository/DeviceRepository.kt
@@ -7,33 +7,30 @@ import extension.waitForRunning
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import model.Device
+import model.Resolution
 
 class DeviceRepository(
     private val adbCommand: AdbCommand,
     private val scrcpyCommand: ScrcpyCommand
 ) {
-    private var selected: Device? = null
     private var process: Process? = null
+    var selected: Device? = null
 
     fun getAll(): List<Device> {
         return adbCommand.getDevices()
     }
 
-    fun select(device: Device) {
-        selected = device
-    }
-
-    fun run(onDestroy: suspend () -> Unit): Boolean {
-        if (selected == null) {
-            return false
-        }
-
+    fun run(
+        device: Device? = null,
+        resolution: Resolution? = null,
+        onDestroy: (suspend () -> Unit)? = null
+    ): Boolean {
         return try {
             process?.destroy()
-            process = scrcpyCommand.run(selected!!)
+            process = scrcpyCommand.run(device, resolution)
             MainScope().launch {
                 process?.waitForRunning(1000)
-                process?.monitor(1000) { onDestroy.invoke() }
+                process?.monitor(1000) { onDestroy?.invoke() }
             }
             true
         } catch (e: Exception) {
