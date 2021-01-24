9abc6436ec7743686ae0df4f0c8087cc02ece885
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-24 16:33:48 +0900

    feat: impl size option for scrcpy command

--- src/src/main/kotlin/command/ScrcpyCommand.kt
+++ src/src/main/kotlin/command/ScrcpyCommand.kt
@@ -1,9 +1,20 @@
 package command
 
 import model.Device
+import model.Resolution
 
 class ScrcpyCommand {
-    fun run(device: Device): Process {
-        return Runtime.getRuntime().exec("scrcpy -s ${device.id}")
+    fun run(device: Device? = null, resolution: Resolution? = null): Process {
+        var command = "scrcpy"
+
+        if (device != null) {
+            command = command.plus(" -s ${device.id}")
+        }
+
+        if (resolution != null) {
+            command = command.plus("-m ${resolution.width}")
+        }
+
+        return Runtime.getRuntime().exec(command)
     }
 }
