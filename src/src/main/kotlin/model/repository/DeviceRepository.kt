445d9d731c62c546a9a331715f966bd56012c4de
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-20 08:34:29 +0900

    feat: change first select device and resolution

--- src/src/main/kotlin/model/repository/DeviceRepository.kt
+++ src/src/main/kotlin/model/repository/DeviceRepository.kt
@@ -6,7 +6,7 @@ import model.entity.Device
 class DeviceRepository(
     private val adbCommand: AdbCommand
 ) {
-    var selected: Device? = null
+    var selected: Device? = getAll().firstOrNull()
 
     fun getAll(): List<Device> {
         return adbCommand.fetchDevices()
