b3ea7f241e0cd108d38c2486696e3e3a7f6003c6
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-20 20:59:28 +0900

    feat: refactor page label

--- src/src/main/kotlin/resource/Navigation.kt
+++ src/src/main/kotlin/resource/Navigation.kt
@@ -3,4 +3,8 @@ package resource
 object Navigation {
     const val CONNECT_PAGE: String = "CONNECT"
     const val SETTING_PAGE: String = "SETTING"
+
+    val PAGE_NAMES: List<String> = listOf(
+        CONNECT_PAGE, SETTING_PAGE
+    )
 }
