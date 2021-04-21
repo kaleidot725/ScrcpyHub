30980c438446c49a2d13172f2844bb667e508bc6
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-22 08:38:29 +0900

    feat: update kotlin and compose

--- src/build.gradle.kts
+++ src/build.gradle.kts
@@ -3,12 +3,12 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.4.20"
-    id("org.jetbrains.compose") version "0.2.0-build132"
+    kotlin("jvm") version "1.4.31"
+    id("org.jetbrains.compose") version "0.3.2"
 }
 
 group = "me.kaleidot725"
-version = "1.0"
+version = "1.0.0"
 
 repositories {
     jcenter()
