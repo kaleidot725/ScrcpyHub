f1a807d22d50f9306e9872a5683e060049310bfe
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-22 08:38:20 +0900

    feat: create MenuPage

--- src/src/main/kotlin/main.kt
+++ src/src/main/kotlin/main.kt
@@ -1,18 +1,13 @@
 import androidx.compose.animation.Crossfade
 import androidx.compose.animation.core.tween
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Row
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.IntSize
-import androidx.compose.ui.unit.dp
 import model.command.AdbCommand
 import model.command.ScrcpyCommand
 import model.repository.DeviceRepository
@@ -22,6 +17,7 @@ import model.usecase.*
 import resource.Navigation
 import resource.Strings
 import view.ConnectPage
+import view.page.MenuPage
 import view.page.SettingPage
 
 fun main() = Window(
@@ -45,34 +41,23 @@ fun main() = Window(
         var selectedPageName by remember { mutableStateOf(Navigation.CONNECT_PAGE) }
 
         Row {
-            Column(modifier = Modifier.width(150.dp).padding(4.dp)) {
-                Navigation.PAGE_NAMES.forEach { pageName ->
-                    Text(
-                        pageName,
-                        Modifier.fillMaxWidth().padding(4.dp).clickable(true) { selectedPageName = pageName },
-                        color = if (selectedPageName == pageName) Color.Blue else Color.Black
-                    )
-                }
-            }
+            MenuPage(
+                menuNames = Navigation.PAGE_NAMES,
+                selectedMenuName = selectedPageName,
+                onSelected = { selectedPageName = it }
+            )
 
-            Column(modifier = Modifier.padding(4.dp)) {
-                Crossfade(selectedPageName, animation = tween(100)) { selectedPageName ->
-                    when (selectedPageName) {
-                        Navigation.CONNECT_PAGE -> {
-                            ConnectPage(
-                                fetchDevicesUseCase,
-                                selectDeviceUseCase,
-                                fetchResolutionsUseCase,
-                                selectResolutionUseCase,
-                                deviceRepository,
-                                resolutionRepository,
-                                startScrcpyUseCase,
-                                stopScrcpyUseCase
-                            )
-                        }
-                        Navigation.SETTING_PAGE -> {
-                            SettingPage()
-                        }
+            Crossfade(selectedPageName, animation = tween(100)) { selectedPageName ->
+                when (selectedPageName) {
+                    Navigation.CONNECT_PAGE -> {
+                        ConnectPage(
+                            fetchDevicesUseCase, selectDeviceUseCase, fetchResolutionsUseCase,
+                            selectResolutionUseCase, deviceRepository, resolutionRepository,
+                            startScrcpyUseCase, stopScrcpyUseCase
+                        )
+                    }
+                    Navigation.SETTING_PAGE -> {
+                        SettingPage()
                     }
                 }
             }
--- src/src/main/kotlin/view/page/ConnectPage.kt
+++ src/src/main/kotlin/view/page/ConnectPage.kt
@@ -1,6 +1,11 @@
 package view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
 import model.repository.DeviceRepository
 import model.repository.ResolutionRepository
 import model.usecase.*
@@ -16,7 +21,15 @@ fun ConnectPage(
     startScrcpyUseCase: StartScrcpyUseCase,
     stopScrcpyUseCase: StopScrcpyUseCase
 ) {
-    DeviceListView(fetchDevicesUseCase, selectDeviceUseCase)
-    ResolutionListView(fetchResolutionsUseCase, selectResolutionUseCase)
-    RunAndStopButton(deviceRepository, resolutionRepository, startScrcpyUseCase, stopScrcpyUseCase)
+    val devices = fetchDevicesUseCase.execute()
+
+    devices.forEach { device ->
+        Column {
+            Text(device.name, style = TextStyle(color = Color.Black, fontSize = 24.sp))
+//            DeviceListView(fetchDevicesUseCase, selectDeviceUseCase)
+//            ResolutionListView(fetchResolutionsUseCase, selectResolutionUseCase)
+//            RunAndStopButton(deviceRepository, resolutionRepository, startScrcpyUseCase, stopScrcpyUseCase)
+        }
+    }
+
 }
