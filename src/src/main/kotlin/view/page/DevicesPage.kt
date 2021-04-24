d3d9c63d5af25938b2a09b33acb88278bfe24945
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-24 10:46:06 +0900

    feat: replace device list

---
+++ src/src/main/kotlin/view/page/DevicesPage.kt
@@ -0,0 +1,24 @@
+package view
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.runtime.*
+import model.usecase.FetchDevicesUseCase
+import model.usecase.StartScrcpyUseCase
+import model.usecase.StopScrcpyUseCase
+
+@Composable
+fun ConnectPage(
+    fetchDevicesUseCase: FetchDevicesUseCase,
+    startScrcpyUseCase: StartScrcpyUseCase,
+    stopScrcpyUseCase: StopScrcpyUseCase
+) {
+    var showMenu by remember { mutableStateOf(false) }
+    var selectedIndex by remember { mutableStateOf(0) }
+
+    val devices = fetchDevicesUseCase.execute()
+    LazyColumn {
+        items(devices, itemContent = { device -> DeviceCard(device, startScrcpyUseCase, stopScrcpyUseCase) })
+    }
+}
+
