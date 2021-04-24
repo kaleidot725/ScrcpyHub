63e2136f45ab7462ad39a8e0970e7f301c9a11ce
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-24 18:26:58 +0900

    feat: add not found device message

--- src/src/main/kotlin/view/page/DevicesPage.kt
+++ src/src/main/kotlin/view/page/DevicesPage.kt
@@ -1,12 +1,23 @@
 package view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.*
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import model.usecase.FetchDevicesUseCase
 import model.usecase.IsRunningScrcpyUseCase
 import model.usecase.StartScrcpyUseCase
 import model.usecase.StopScrcpyUseCase
+import resource.Strings
 
 @Composable
 fun ConnectPage(
@@ -15,21 +26,30 @@ fun ConnectPage(
     stopScrcpyUseCase: StopScrcpyUseCase,
     isRunningScrcpyUseCase: IsRunningScrcpyUseCase
 ) {
-    var showMenu by remember { mutableStateOf(false) }
-    var selectedIndex by remember { mutableStateOf(0) }
-
     val devices = fetchDevicesUseCase.execute()
-    LazyColumn {
-        items(
-            devices,
-            itemContent = { device ->
-                DeviceCard(
-                    device,
-                    startScrcpyUseCase,
-                    stopScrcpyUseCase,
-                    isRunningScrcpyUseCase
-                )
-            })
+    if (devices.isEmpty()) {
+        Box(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+            Text(
+                Strings.NOT_FOUND_ANDROID_DEVICES,
+                style = TextStyle(color = Color.Black, fontSize = 20.sp),
+                modifier = Modifier.align(Alignment.Center)
+            )
+        }
+    } else {
+        LazyColumn {
+            items(
+                devices,
+                itemContent = { device ->
+                    DeviceCard(
+                        device,
+                        startScrcpyUseCase,
+                        stopScrcpyUseCase,
+                        isRunningScrcpyUseCase
+                    )
+                }
+            )
+        }
     }
+
 }
 
