98b1c02519815c05be4014a10691d6965534b24d
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-24 18:27:53 +0900

    feat: remove unuse Strings

--- src/src/main/kotlin/resource/Strings.kt
+++ src/src/main/kotlin/resource/Strings.kt
@@ -2,10 +2,7 @@ package resource
 
 object Strings {
     const val APP_NAME = "ScrcpyHub"
-    const val RESOLUTION_TITLE = "Resolution"
-    const val DEVICE_TITLE = "Device"
     const val RUN = "Run"
     const val STOP = "Stop"
-    const val NONE = "None"
     const val NOT_FOUND_ANDROID_DEVICES = "Not found android devices."
 }
