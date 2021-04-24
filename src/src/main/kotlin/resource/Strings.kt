da3ea8a9c82248a9bcb953c2894298c067495652
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-25 06:16:35 +0900

    WIP

--- src/src/main/kotlin/resource/Strings.kt
+++ src/src/main/kotlin/resource/Strings.kt
@@ -2,9 +2,8 @@ package resource
 
 object Strings {
     const val APP_NAME = "ScrcpyHub"
-    const val RUN = "Run"
-    const val STOP = "Stop"
+    const val RUN = "START"
+    const val STOP = "STOP"
     const val NOT_FOUND_ANDROID_DEVICES = "Not found android devices."
     const val NOT_FOUND_SETTING = "Not found settings."
-
 }
