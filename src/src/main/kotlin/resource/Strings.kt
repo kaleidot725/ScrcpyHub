a02b1d5b542a9bc4c0e7386acc27428dbbea6c79
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-24 18:30:48 +0900

    feat: add not found setting mesasge

--- src/src/main/kotlin/resource/Strings.kt
+++ src/src/main/kotlin/resource/Strings.kt
@@ -5,4 +5,6 @@ object Strings {
     const val RUN = "Run"
     const val STOP = "Stop"
     const val NOT_FOUND_ANDROID_DEVICES = "Not found android devices."
+    const val NOT_FOUND_SETTING = "Not found settings."
+
 }
