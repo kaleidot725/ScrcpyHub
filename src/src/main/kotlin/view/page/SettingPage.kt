dc905f3a4afdffb54b57ce8e9556e5b46f5d9ee0
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-24 18:30:48 +0900

    feat: add not found setting mesasge

--- src/src/main/kotlin/view/page/SettingPage.kt
+++ src/src/main/kotlin/view/page/SettingPage.kt
@@ -1,9 +1,25 @@
 package view.page
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import resource.Strings
 
 @Composable
 fun SettingPage() {
-    Text("SETTING PAGE")
+    Box(modifier = Modifier.fillMaxSize().padding(8.dp)) {
+        Text(
+            Strings.NOT_FOUND_SETTING,
+            style = TextStyle(color = Color.Black, fontSize = 20.sp),
+            modifier = Modifier.align(Alignment.Center)
+        )
+    }
 }
