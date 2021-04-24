d21b7bf865c278afe2d64b02c57dd4403f9f28de
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-24 18:38:08 +0900

    feat: refactoring view

--- src/src/main/kotlin/view/DevicesPage.kt
+++ src/src/main/kotlin/view/DevicesPage.kt
@@ -1,18 +1,19 @@
 package view
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Button
+import androidx.compose.material.Card
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import model.entity.Device
 import model.usecase.FetchDevicesUseCase
 import model.usecase.IsRunningScrcpyUseCase
 import model.usecase.StartScrcpyUseCase
@@ -51,5 +52,40 @@ fun ConnectPage(
         }
     }
 
+}
+
+@Composable
+fun DeviceCard(
+    device: Device,
+    startScrcpyUseCase: StartScrcpyUseCase,
+    stopScrcpyUseCase: StopScrcpyUseCase,
+    isRunningScrcpyUseCase: IsRunningScrcpyUseCase
+) {
+    var running by remember { mutableStateOf(isRunningScrcpyUseCase.execute(device)) }
+
+    Card(modifier = Modifier.wrapContentHeight().fillMaxWidth().padding(start = 8.dp, end = 8.dp, bottom = 8.dp)) {
+        Box(modifier = Modifier.padding(8.dp)) {
+            Button(
+                onClick = {
+                    if (running) {
+                        running = false
+                        stopScrcpyUseCase.execute(device)
+                    } else {
+                        running = true
+                        startScrcpyUseCase.execute(device, null, onDestroy = { running = false })
+                    }
+                },
+                modifier = Modifier.wrapContentSize().align(Alignment.BottomEnd)
+            ) {
+                Text(if (running) Strings.STOP else Strings.RUN)
+            }
+
+            Text(
+                device.id,
+                style = TextStyle(color = Color.Black, fontSize = 20.sp),
+                modifier = Modifier.wrapContentSize().align(Alignment.CenterStart)
+            )
+        }
+    }
 }
 
