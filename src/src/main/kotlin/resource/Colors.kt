875e8940aa1d0dbad1aab8ba98c20f482ce59f90
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-28 09:31:17 +0900

    feat: update theme

--- src/src/main/kotlin/resource/Colors.kt
+++ src/src/main/kotlin/resource/Colors.kt
@@ -3,5 +3,6 @@ package resource
 import androidx.compose.ui.graphics.Color
 
 object Colors {
-    val SMOKE_WHITE = Color(245, 245,245)
+    val SMOKE_WHITE = Color(245, 245, 245)
+    val NAVY = Color(26, 35, 126)
 }
---
+++ src/src/main/kotlin/view/MainTheme.kt
@@ -0,0 +1,27 @@
+package view
+
+import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun MainTheme(content: @Composable () -> Unit) {
+    MaterialTheme(content = content, colors = colors)
+}
+
+private val colors =  Colors(
+    primary = resource.Colors.NAVY,
+    primaryVariant = resource.Colors.NAVY,
+    background = Color.White,
+    surface = Color.White,
+    error = Color.Red,
+    onPrimary = Color.White,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    onError = Color.Black,
+    secondary = resource.Colors.NAVY,
+    secondaryVariant = resource.Colors.NAVY,
+    onSecondary = Color.White,
+    isLight = true,
+)
