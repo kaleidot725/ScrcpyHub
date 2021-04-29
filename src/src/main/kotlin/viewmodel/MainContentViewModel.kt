b3eae6b83738c95737b8216628b79f8614347f5c
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-04-29 10:34:22 +0900

    feat: modify root class package

--- src/src/main/kotlin/viewmodel/MainContentViewModel.kt
+++ src/src/main/kotlin/viewmodel/MainContentViewModel.kt
@@ -3,15 +3,14 @@ package viewmodel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import resource.Navigation
-import resource.Root
 
 class MainContentViewModel : ViewModel() {
-    val pages: StateFlow<List<Root>> = MutableStateFlow(Navigation.PAGE_NAMES)
+    val pages: StateFlow<List<Navigation.Root>> = MutableStateFlow(Navigation.PAGE_NAMES)
 
-    private val _selectedPages: MutableStateFlow<Root> = MutableStateFlow(Navigation.DEVICES_PAGE)
-    val selectedPages: StateFlow<Root> = _selectedPages
+    private val _selectedPages: MutableStateFlow<Navigation.Root> = MutableStateFlow(Navigation.DEVICES_PAGE)
+    val selectedPages: StateFlow<Navigation.Root> = _selectedPages
 
-    fun selectPage(page: Root) {
+    fun selectPage(page: Navigation.Root) {
         _selectedPages.value = page
     }
 }
