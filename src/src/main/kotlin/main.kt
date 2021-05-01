67afdbdea1b1090908cb891b0a533b6dcd773002
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-05-01 15:31:32 +0900

    feat: add window icon

--- src/src/main/kotlin/main.kt
+++ src/src/main/kotlin/main.kt
@@ -4,8 +4,16 @@ import model.di.appModule
 import org.koin.core.context.GlobalContext.startKoin
 import resource.Strings
 import view.MainContent
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
 
-fun main() = Window(resizable = false, size = IntSize(350, 550), title = Strings.APP_NAME) {
+fun main() = Window(
+    resizable = false,
+    size = IntSize(350, 550),
+    title = Strings.APP_NAME,
+    icon = getWindowIcon()
+) {
     setupModule()
     MainContent()
 }
@@ -15,4 +23,19 @@ private fun setupModule() {
         printLogger()
         modules(appModule)
     }
+}
+
+private fun getWindowIcon(): BufferedImage {
+    var image: BufferedImage? = null
+    try {
+        image = ImageIO.read(File("icon.png"))
+    } catch (e: Exception) {
+        // image file does not exist
+    }
+
+    if (image == null) {
+        image = BufferedImage(1, 1, BufferedImage.TYPE_INT_RGB)
+    }
+
+    return image
 }
