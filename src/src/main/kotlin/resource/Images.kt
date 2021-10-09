77acce0fe606c3d053cd1e6304996afd20a7b589
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-10-09 16:57:27 +0900

    feat: fix tray icon and action

--- src/src/main/kotlin/resource/Images.kt
+++ src/src/main/kotlin/resource/Images.kt
@@ -1,7 +1,8 @@
 package resource
 
 object Images {
-    const val DEVICE_BLACK = "device.png"
+    const val TRAY = "tray.svg"
+    const val DEVICE_BLACK = "device.svg"
     const val SETTING = "setting.svg"
     const val CLOSE = "close.svg"
 }
