3424181fee29a1bfc7dc86450b887c60915d5411
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-10-16 20:24:55 +0900

    feat: refactor SettingPage

--- src/src/main/kotlin/view/page/SettingPage.kt
+++ src/src/main/kotlin/view/page/SettingPage.kt
@@ -1,5 +1,6 @@
 package view.page
 
+import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Card
@@ -51,44 +52,25 @@ private fun onDrawPage(
             onAction = { onNavigateDevice?.invoke() }
         )
 
-        Card(modifier = Modifier.padding(horizontal = 8.dp)) {
-            Column(modifier = Modifier.padding(8.dp)) {
-                Text(ADB_LOCATION, fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
-                Text(IF_ADB_LOCATION_IS_EMPTY, fontSize = 12.sp, color = Color.Gray, fontWeight = FontWeight.SemiBold)
-                Spacer(modifier = Modifier.height(8.dp))
-                TextField(
-                    value = adbLocation,
-                    modifier = Modifier.fillMaxWidth(),
-                    onValueChange = {
-                        viewModel.updateAdbLocation(it)
-                    }
-                )
-            }
-        }
+        AdbLocationSetting(
+            adbLocation = adbLocation,
+            onUpdate = { viewModel.updateAdbLocation(it) },
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(
+            modifier = Modifier.height(8.dp)
+        )
 
-        Card(modifier = Modifier.padding(horizontal = 8.dp)) {
-            Column(modifier = Modifier.padding(8.dp)) {
-                Text(SCRCPY_LOCATION, fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
-                Text(
-                    IF_SCRCPY_LOCATION_IS_EMPTY,
-                    fontSize = 12.sp,
-                    color = Color.Gray,
-                    fontWeight = FontWeight.SemiBold
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                TextField(
-                    value = scrcpyLocation,
-                    modifier = Modifier.fillMaxWidth(),
-                    onValueChange = {
-                        viewModel.updateScrcpyLocation(it)
-                    }
-                )
-            }
-        }
+        ScrcpyLocationSetting(
+            scrcpyLocation = scrcpyLocation,
+            onUpdate = { viewModel.updateScrcpyLocation(it) },
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
 
-        Spacer(modifier = Modifier.height(8.dp))
+        Spacer(
+            modifier = Modifier.height(8.dp)
+        )
 
         Box(modifier = Modifier.padding(horizontal = 8.dp)) {
             Button(
@@ -99,4 +81,53 @@ private fun onDrawPage(
             }
         }
     }
+}
+
+@Composable
+private fun AdbLocationSetting(adbLocation: String, onUpdate: (String) -> Unit, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(ADB_LOCATION, fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
+            Text(IF_ADB_LOCATION_IS_EMPTY, fontSize = 12.sp, color = Color.Gray, fontWeight = FontWeight.SemiBold)
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = adbLocation,
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = { onUpdate(it) }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun AdbLocationSetting_Preview() {
+    AdbLocationSetting("TEST", {})
+}
+
+@Composable
+private fun ScrcpyLocationSetting(scrcpyLocation: String, onUpdate: (String) -> Unit, modifier: Modifier = Modifier) {
+    Card(modifier = modifier) {
+        Column(modifier = Modifier.padding(8.dp)) {
+            Text(SCRCPY_LOCATION, fontSize = 16.sp, fontWeight = FontWeight.SemiBold)
+            Text(
+                IF_SCRCPY_LOCATION_IS_EMPTY,
+                fontSize = 12.sp,
+                color = Color.Gray,
+                fontWeight = FontWeight.SemiBold
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            TextField(
+                value = scrcpyLocation,
+                modifier = Modifier.fillMaxWidth(),
+                onValueChange = { onUpdate(it) }
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+private fun ScrcpyLocationSetting_Preview() {
+    ScrcpyLocationSetting("TEST", {})
 }
