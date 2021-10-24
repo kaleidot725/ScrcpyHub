46f5b7c3d01a74d849cf5890396677a15bde8902
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-10-24 15:52:34 +0900

    chore: update version (v1.1.0)

--- src/src/main/kotlin/view/components/AppWindow.kt
+++ src/src/main/kotlin/view/components/AppWindow.kt
@@ -3,10 +3,12 @@ package view.components
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.FrameWindowScope
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.WindowState
+import resource.Images
 
 @Composable
 fun AppWindow(onCloseRequest: () -> Unit, state: WindowState, content: @Composable FrameWindowScope.() -> Unit) {
@@ -15,7 +17,8 @@ fun AppWindow(onCloseRequest: () -> Unit, state: WindowState, content: @Composab
         state = state,
         resizable = false,
         undecorated = true,
-        transparent = true
+        transparent = true,
+        icon = painterResource(Images.DEVICE)
     ) {
         Card(shape = RoundedCornerShape(8.dp)) { content.invoke(this) }
     }
