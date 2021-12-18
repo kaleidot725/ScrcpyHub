a3f0fe57de73b5e3c7e2d0abb9be5bc4a2b0130f
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-12-19 08:32:45 +0900

    style: format code

--- src/main/kotlin/view/components/DeviceCard.kt
+++ src/main/kotlin/view/components/DeviceCard.kt
@@ -85,22 +85,26 @@ fun DeviceCard(
                         ProcessStatus.RUNNING -> stopScrcpy?.invoke(context)
                         ProcessStatus.RECORDING -> {}
                     }
-                }, colors = when (processStatus) {
+                },
+                colors = when (processStatus) {
                     ProcessStatus.IDLE -> ButtonDefaults.buttonColors(MaterialTheme.colors.primary)
                     ProcessStatus.RUNNING -> ButtonDefaults.buttonColors(MaterialTheme.colors.primary)
                     ProcessStatus.RECORDING -> ButtonDefaults.buttonColors(Color.Red)
-                }, modifier = Modifier.wrapContentHeight().width(80.dp).align(Alignment.CenterVertically)
+                },
+                modifier = Modifier.wrapContentHeight().width(80.dp).align(Alignment.CenterVertically)
             ) {
                 Text(
                     text = when (processStatus) {
                         ProcessStatus.IDLE -> Strings.DEVICES_PAGE_START
                         ProcessStatus.RUNNING -> Strings.DEVICES_PAGE_STOP
                         ProcessStatus.RECORDING -> Strings.DEVICES_PAGE_RECORDING
-                    }, color = Color.White, style = MaterialTheme.typography.button
+                    },
+                    color = Color.White, style = MaterialTheme.typography.button
                 )
             }
 
-            DeviceDropDownMenu(processStatus = processStatus,
+            DeviceDropDownMenu(
+                processStatus = processStatus,
                 onSetting = { goToDetail?.invoke(context) },
                 onScreenShot = { takeScreenshot?.invoke(context) },
                 onStartRecording = { startRecording?.invoke(context) },
@@ -123,9 +127,11 @@ private fun DeviceDropDownMenu(
     var expanded by remember { mutableStateOf(false) }
 
     Box(modifier = modifier) {
-        Image(painter = painterResource(Images.DOTS),
+        Image(
+            painter = painterResource(Images.DOTS),
             contentDescription = "",
-            modifier = Modifier.size(30.dp).clickable { expanded = true })
+            modifier = Modifier.size(30.dp).clickable { expanded = true }
+        )
 
         DropdownMenu(expanded = expanded, onDismissRequest = { expanded = false }) {
             DropdownMenuItem(
@@ -133,59 +139,62 @@ private fun DeviceDropDownMenu(
                     onSetting()
                     expanded = false
                 }, modifier = Modifier.height(32.dp)
-            ) {
-                Text(
-                    text = Strings.DEVICE_DROP_DOWN_PREFERENCE_MENU_TITLE, style = MaterialTheme.typography.body2
-                )
-            }
+                ) {
+                    Text(
+                        text = Strings.DEVICE_DROP_DOWN_PREFERENCE_MENU_TITLE, style = MaterialTheme.typography.body2
+                    )
+                }
 
-            DropdownMenuItem(
-                onClick = {
-                    onScreenShot()
-                    expanded = false
-                }, modifier = Modifier.height(32.dp)
-            ) {
-                Text(
-                    text = Strings.DEVICE_DROP_DOWN_SCREEN_SHOT_MENU_TITLE, style = MaterialTheme.typography.body2
-                )
-            }
-
-            DropdownMenuItem(
-                enabled = when (processStatus) {
-                    ProcessStatus.IDLE -> true
-                    ProcessStatus.RUNNING -> false
-                    ProcessStatus.RECORDING -> true
-                }, onClick = {
-                    when (processStatus) {
-                        ProcessStatus.IDLE -> onStartRecording.invoke()
-                        ProcessStatus.RUNNING -> Unit
-                        ProcessStatus.RECORDING -> onStopRecording.invoke()
+                DropdownMenuItem(
+                    onClick = {
+                        onScreenShot()
+                        expanded = false
+                    }, modifier = Modifier.height(32.dp)
+                    ) {
+                        Text(
+                            text = Strings.DEVICE_DROP_DOWN_SCREEN_SHOT_MENU_TITLE, style = MaterialTheme.typography.body2
+                        )
                     }
-                    expanded = false
-                }, modifier = Modifier.height(32.dp)
-            ) {
-                Text(
-                    text = when (processStatus) {
-                        ProcessStatus.IDLE -> Strings.DEVICE_DROP_DOWN_START_RECORDING_MENU_TITLE
-                        ProcessStatus.RUNNING -> Strings.DEVICE_DROP_DOWN_START_RECORDING_MENU_TITLE
-                        ProcessStatus.RECORDING -> Strings.DEVICE_DROP_DOWN_STOP_RECORDING_MENU_TITLE
-                    }, style = MaterialTheme.typography.body2
-                )
+
+                    DropdownMenuItem(
+                        enabled = when (processStatus) {
+                            ProcessStatus.IDLE -> true
+                            ProcessStatus.RUNNING -> false
+                            ProcessStatus.RECORDING -> true
+                        },
+                        onClick = {
+                            when (processStatus) {
+                                ProcessStatus.IDLE -> onStartRecording.invoke()
+                                ProcessStatus.RUNNING -> Unit
+                                ProcessStatus.RECORDING -> onStopRecording.invoke()
+                            }
+                            expanded = false
+                        }, modifier = Modifier.height(32.dp)
+                        ) {
+                            Text(
+                                text = when (processStatus) {
+                                    ProcessStatus.IDLE -> Strings.DEVICE_DROP_DOWN_START_RECORDING_MENU_TITLE
+                                    ProcessStatus.RUNNING -> Strings.DEVICE_DROP_DOWN_START_RECORDING_MENU_TITLE
+                                    ProcessStatus.RECORDING -> Strings.DEVICE_DROP_DOWN_STOP_RECORDING_MENU_TITLE
+                                },
+                                style = MaterialTheme.typography.body2
+                            )
+                        }
+                    }
+                }
             }
-        }
-    }
-}
 
-@Preview
-@Composable
-private fun DeviceCard_Preview() {
-    DeviceCard(Device.Context(Device("ID", "NAME")), ProcessStatus.IDLE)
-}
+            @Preview
+            @Composable
+            private fun DeviceCard_Preview() {
+                DeviceCard(Device.Context(Device("ID", "NAME")), ProcessStatus.IDLE)
+            }
 
-@Preview
-@Composable
-private fun DeviceCard_Preview_Overflow() {
-    val id = "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
-    val name = "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
-    DeviceCard(Device.Context(Device(id, name)), ProcessStatus.IDLE)
-}
+            @Preview
+            @Composable
+            private fun DeviceCard_Preview_Overflow() {
+                val id = "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                val name = "012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789"
+                DeviceCard(Device.Context(Device(id, name)), ProcessStatus.IDLE)
+            }
+            
