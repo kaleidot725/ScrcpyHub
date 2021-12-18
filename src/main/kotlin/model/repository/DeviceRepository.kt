a235a128a761c452cd2b0ad28088c3771d319694
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-12-19 06:51:41 +0900

    style: addapt ktlint format

--- src/main/kotlin/model/repository/DeviceRepository.kt
+++ src/main/kotlin/model/repository/DeviceRepository.kt
@@ -59,14 +59,14 @@ class DeviceRepository(private val home: String) {
         val date = ZonedDateTime
             .now(ZoneId.systemDefault())
             .format(DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss"))
-        return "${System.getProperty("user.home")}/Desktop/${context.displayName}-${date}.png"
+        return "${System.getProperty("user.home")}/Desktop/${context.displayName}-$date.png"
     }
 
     fun createRecordPathForDesktop(context: Device.Context): String {
         val date = ZonedDateTime
             .now(ZoneId.systemDefault())
             .format(DateTimeFormatter.ofPattern("yyyy_MM_dd_HH_mm_ss"))
-        return "${System.getProperty("user.home")}/Desktop/${context.displayName}-${date}.mp4"
+        return "${System.getProperty("user.home")}/Desktop/${context.displayName}-$date.mp4"
     }
 
     private fun writeCache(context: Device.Context) {
