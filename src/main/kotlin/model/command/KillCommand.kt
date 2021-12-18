2ad45689099fc8ebc6f1d45409ff5bb185369fc4
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-12-18 15:23:52 +0900

    feat: replace kill command

--- src/main/kotlin/model/command/KillCommand.kt
+++ src/main/kotlin/model/command/KillCommand.kt
@@ -3,7 +3,7 @@ package model.command
 import model.command.factory.KillCommandFactory
 
 class KillCommand(private val factory: KillCommandFactory) {
-    fun run(pid: Int): Process {
+    fun run(pid: Long): Process {
         val command = factory.create(pid)
         return ProcessBuilder(command).start()
     }
