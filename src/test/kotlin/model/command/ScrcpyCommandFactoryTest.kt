ab4e76d10191a50a63abe94b8f45def5ebc6f75b
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-12-19 07:37:51 +0900

    feat: add automatically separator

--- src/test/kotlin/model/command/ScrcpyCommandFactoryTest.kt
+++ src/test/kotlin/model/command/ScrcpyCommandFactoryTest.kt
@@ -9,12 +9,15 @@ class ScrcpyCommandFactoryTest : StringSpec(
     {
         "create" {
             val factory = ScrcpyCommandFactory(path = "test/")
+            val factoryWhenNoSeparator = ScrcpyCommandFactory(path = "test")
 
             val device1 = Device(id = "DEVICE1", name = "NAME", customName = "CUSTOM_NAME", maxSize = null)
             factory.create(device1) shouldBe listOf("test/scrcpy", "-s", "DEVICE1")
+            factoryWhenNoSeparator.create(device1) shouldBe listOf("test/scrcpy", "-s", "DEVICE1")
 
             val device2 = Device(id = "DEVICE2", name = "NAME", customName = "CUSTOM_NAME", maxSize = 1000)
             factory.create(device2) shouldBe listOf("test/scrcpy", "-s", "DEVICE2", "-m", "1000")
+            factoryWhenNoSeparator.create(device2) shouldBe listOf("test/scrcpy", "-s", "DEVICE2", "-m", "1000")
         }
         "create_when_no_path_specified" {
             val factory = ScrcpyCommandFactory()
@@ -27,7 +30,10 @@ class ScrcpyCommandFactoryTest : StringSpec(
         }
         "create_help" {
             val factory = ScrcpyCommandFactory(path = "test/")
+            val factoryWhenNoSeparator = ScrcpyCommandFactory(path = "test")
+
             factory.createHelp() shouldBe listOf("test/scrcpy", "-h")
+            factoryWhenNoSeparator.createHelp() shouldBe listOf("test/scrcpy", "-h")
         }
         "create_help_when_no_path_specified" {
             val factory = ScrcpyCommandFactory()
