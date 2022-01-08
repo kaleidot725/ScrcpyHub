3f87d8b12ee941e3c6f9947526f68775635c8d0c
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-08 13:46:53 +0900

    feat: support dark theme

--- src/main/kotlin/resource/Images.kt
+++ src/main/kotlin/resource/Images.kt
@@ -1,13 +1,9 @@
 package resource
 
 object Images {
-    const val REFRESH = "refresh.svg"
     const val TRAY = "tray.svg"
     const val DOTS = "dots.svg"
     const val DEVICE = "device.svg"
     const val SETTING = "setting.svg"
     const val CLOSE = "close.svg"
-    const val PLAY = "play.svg"
-    const val RECORD = "record.svg"
-    const val STOP = "stop.svg"
 }
