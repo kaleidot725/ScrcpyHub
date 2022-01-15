80c3deeb43e53c627b7a492f3c8fb17ba64b50f6
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-01-15 20:51:12 +0900

    feat: create OSContext etc.

--- src/main/kotlin/model/command/factory/KillCommandFactory.kt
+++ src/main/kotlin/model/command/factory/KillCommandFactory.kt
@@ -1,12 +1,23 @@
 package model.command.factory
 
-import common.OsType
+import model.os.OSContext
+import model.os.OSType
 
 interface KillCommandFactory {
     fun create(pid: Long): List<String>
 
     private class KillCommandFactoryForMac : KillCommandFactory {
         @OptIn(kotlin.ExperimentalStdlibApi::class)
+        override fun create(pid: Long): List<String> {
+            return buildList {
+                add("kill")
+                add("-SIGINT")
+                add(pid.toString())
+            }
+        }
+    }
+
+    private class KillCommandFactoryForLinux : KillCommandFactory {
         override fun create(pid: Long): List<String> {
             return buildList {
                 add("kill")
@@ -28,10 +39,11 @@ interface KillCommandFactory {
     }
 
     companion object {
-        fun get(osType: OsType): KillCommandFactory {
-            return when (osType) {
-                OsType.MAC_OS -> KillCommandFactoryForMac()
-                OsType.WINDOWS -> KillCommandFactoryForWindows()
+        fun create(context: OSContext): KillCommandFactory {
+            return when (context.type) {
+                OSType.MAC_OS -> KillCommandFactoryForMac()
+                OSType.LINUX -> KillCommandFactoryForLinux()
+                OSType.WINDOWS -> KillCommandFactoryForWindows()
             }
         }
     }
