7e4c6982a911ebdaa819ebaa35939f15d1d8143b
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-02-19 22:11:31 +0900

    Add text composable

---
+++ src/main/kotlin/view/components/atoms/Texts.kt
@@ -0,0 +1,89 @@
+package view.components.atoms
+
+import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+object Texts {
+    @Composable
+    fun H1(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.h1)
+    }
+
+    @Composable
+    fun H2(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.h2)
+    }
+
+    @Composable
+    fun H3(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.h3)
+    }
+
+    @Composable
+    fun H4(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.h4)
+    }
+
+    @Composable
+    fun H5(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.h5)
+    }
+
+    @Composable
+    fun H6(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.h6)
+    }
+
+    @Composable
+    fun Subtitle1(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.subtitle1)
+    }
+
+    @Composable
+    fun Subtitle2(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.subtitle2)
+    }
+
+    @Composable
+    fun Body1(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.body1)
+    }
+
+    @Composable
+    fun Body2(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.body2)
+    }
+
+    @Composable
+    fun Button(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.button)
+    }
+
+    @Composable
+    fun Caption(text: String, modifier: Modifier = Modifier) {
+        Text(text, modifier, style = MaterialTheme.typography.caption)
+    }
+}
+
+@Preview
+@Composable
+private fun Texts_Preview() {
+    Column {
+        Texts.H1("H1")
+        Texts.H2("H2")
+        Texts.H3("H3")
+        Texts.H4("H4")
+        Texts.H5("H5")
+        Texts.H6("H6")
+        Texts.Subtitle1("Subtitle1")
+        Texts.Subtitle2("Subtitle2")
+        Texts.Body1("Body1")
+        Texts.Body2("Body2")
+        Texts.Button("Button")
+        Texts.Caption("Caption")
+    }
+}
