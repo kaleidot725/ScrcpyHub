30d1ddf0cf3367f95231a5ab573bdfcf6dfdce49
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-07-13 21:51:47 +0900

    Update dependencies version

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,14 +2,14 @@ import org.jetbrains.compose.compose
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 group = "me.kaleidot725"
-version = "1.6.0"
+version = "1.7.0"
 
 plugins {
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
 
     id("org.jetbrains.compose") version "1.0.1"
-    id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
+    id("org.jlleitschuh.gradle.ktlint") version "10.3.0"
 }
 
 repositories {
