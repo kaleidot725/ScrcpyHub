4592fc983a5d46e1ac50b0a99ed83a2dc1404d5d
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-12-17 17:38:42 +0900

    Refactor devices loading screen

---
+++ src/main/kotlin/view/pages/devices/DevicesPageState.kt
@@ -0,0 +1,13 @@
+package view.pages.devices
+
+import model.entity.Device
+import model.repository.ProcessStatus
+
+sealed class DevicesPageState {
+    object Loading : DevicesPageState()
+    object DeviceIsEmpty : DevicesPageState()
+    data class DeviceExist(val devices: List<DeviceStatus>) : DevicesPageState()
+    object Error : DevicesPageState()
+}
+
+data class DeviceStatus(val context: Device.Context, val processStatus: ProcessStatus)
