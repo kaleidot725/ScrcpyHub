b3d197ece8ddd85ae9a667076bbf6fd53d0b9202
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2022-12-25 16:49:49 +0900

    Fix headers

--- src/main/kotlin/view/parts/TopPageHeader.kt
+++ src/main/kotlin/view/parts/TopPageHeader.kt
@@ -1,28 +1,33 @@
 package view.parts
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.window.WindowDraggableArea
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.WindowScope
 
 @Composable
-fun TopPageHeader(windowScope: WindowScope, title: String, optionContent: @Composable () -> Unit) {
+fun TopPageHeader(windowScope: WindowScope, title: String, onClickOption: () -> Unit) {
     windowScope.WindowDraggableArea {
-        Row(
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .height(48.dp)
@@ -35,13 +40,23 @@ fun TopPageHeader(windowScope: WindowScope, title: String, optionContent: @Compo
                 textAlign = TextAlign.Center,
                 modifier = Modifier
                     .wrapContentHeight()
-                    .weight(0.95f)
-                    .align(Alignment.CenterVertically),
+                    .align(Alignment.Center),
                 maxLines = 1,
                 overflow = TextOverflow.Ellipsis,
                 color = Color.White
             )
-            Box(modifier = Modifier.align(Alignment.CenterVertically)) { optionContent() }
+
+            IconButton(
+                onClick = onClickOption,
+                modifier = Modifier.padding(top = 2.dp).align(Alignment.CenterEnd)
+            ) {
+                Image(
+                    imageVector = Icons.Default.Settings,
+                    contentDescription = "",
+                    contentScale = ContentScale.FillHeight,
+                    colorFilter = ColorFilter.tint(Color.White)
+                )
+            }
         }
     }
 }
