ea6819293b789b18c1934130a6c698bebd13a4e4
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-01-03 13:40:54 +0900

    Fix

--- src/main/kotlin/model/os/OSContext.kt
+++ src/main/kotlin/model/os/OSContext.kt
@@ -16,12 +16,12 @@ class OSContextForMac : OSContext {
 
 class OSContextForLinux : OSContext {
     override val type: OSType = OSType.LINUX
-    override val settingPath: String = System.getProperty("user.home") + "/AppData/ScrcpyHub"
+    override val settingPath: String = System.getProperty("user.home") + "/.config/ScrcpyHub"
     override val desktopPath: String = System.getProperty("user.home") + "/Desktop/"
 }
 
 class OSContextForWindows : OSContext {
     override val type: OSType = OSType.WINDOWS
-    override val settingPath: String = System.getProperty("user.home") + "/.config/ScrcpyHub/"
+    override val settingPath: String = System.getProperty("user.home") + "/AppData/Local/ScrcpyHub/"
     override val desktopPath: String = System.getProperty("user.home") + "/Desktop/"
 }
