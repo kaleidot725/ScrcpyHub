947446fcf452856e533f9d978c123830e7f2ae71
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-02-23 16:34:30 +0900

    Add orientation ui

--- src/main/kotlin/model/entity/Device.kt
+++ src/main/kotlin/model/entity/Device.kt
@@ -14,5 +14,13 @@ data class Device(val id: String = "") {
         val bitrate: Int? = null
     ) {
         val displayName get() = if (customName.isNullOrEmpty()) device.id else customName
+
+        enum class LockOrientation(val title: String, val value: Int?) {
+            NONE("None", null),
+            NATURAL("Natural", 0),
+            COUNTER_CLOCK_WISE_90("90° Counter Clock Wise", 1),
+            CLOCK_WISE_180("180°", 2),
+            CLOCK_WISE_90("90° Clock Wise", 3),
+        }
     }
 }
