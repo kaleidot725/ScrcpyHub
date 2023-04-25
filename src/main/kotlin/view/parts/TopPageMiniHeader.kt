d49e14ddf8d684ccbef32971c4fd0247aa2eda60
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2023-04-26 00:42:30 +0900

    Format by ktlint

--- src/main/kotlin/view/parts/TopPageMiniHeader.kt
+++ src/main/kotlin/view/parts/TopPageMiniHeader.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.window.WindowDraggableArea
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
