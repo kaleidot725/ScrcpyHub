3c9bd7d62f82f3168b7f96a58e61ec6ec10c9497
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-08-31 20:54:09 +0900

    feat: add dirctory setting window

--- src/main/kotlin/view/parts/TextFieldAndError.kt
+++ src/main/kotlin/view/parts/TextFieldAndError.kt
@@ -1,14 +1,23 @@
 package view.parts
 
 import androidx.compose.desktop.ui.tooling.preview.Preview
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 
@@ -19,6 +28,8 @@ fun TextFieldAndError(
     inputText: String,
     onUpdateInputText: (String) -> Unit,
     error: String = "",
+    trailingIcon: ImageVector? = null,
+    onClickTrailingIcon: () -> Unit = {},
     modifier: Modifier = Modifier
 ) {
     Column(modifier = modifier) {
@@ -29,7 +40,21 @@ fun TextFieldAndError(
             maxLines = 1,
             label = { Text(label) },
             placeholder = { Text(placeHolder) },
-            isError = error.isNotEmpty()
+            isError = error.isNotEmpty(),
+            trailingIcon = trailingIcon?.let {
+                {
+                    Image(
+                        imageVector = trailingIcon,
+                        colorFilter = ColorFilter.tint(MaterialTheme.colors.secondary),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .clip(CircleShape)
+                            .indication(MutableInteractionSource(), rememberRipple())
+                            .clickable(onClick = { onClickTrailingIcon() } )
+                            .padding(4.dp)
+                    )
+                }
+            }
         )
 
         if (error.isNotEmpty()) {
